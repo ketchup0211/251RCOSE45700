cd74038a3e70d23f5671cd392f71b614472e7095
CHUMMY_CHAM(trueDOM) <dlcka1111@naver.com>
2025-05-28 21:07:50 +0900

    [FIX] token spawn error

--- Assets/Scripts/Mechanics/TokenController.cs
+++ Assets/Scripts/Mechanics/TokenController.cs
@@ -2,27 +2,16 @@ using UnityEngine;
 
 namespace Platformer.Mechanics
 {
-    /// <summary>
-    /// This class animates all token instances in a scene.
-    /// This allows a single update call to animate hundreds of sprite 
-    /// animations.
-    /// If the tokens property is empty, it will automatically find and load 
-    /// all token instances in the scene at runtime.
-    /// </summary>
     public class TokenController : MonoBehaviour
     {
-        [Tooltip("Frames per second at which tokens are animated.")]
         public float frameRate = 12;
-
-        [Tooltip("Instances of tokens which are animated. If empty, token instances are found and loaded at runtime.")]
         public TokenInstance[] tokens;
-
         float nextFrameTime = 0;
 
         [ContextMenu("Find All Tokens")]
         void FindAllTokensInScene()
         {
-            tokens = UnityEngine.Object.FindObjectsByType<TokenInstance>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+            tokens = UnityEngine.Object.FindObjectsByType<TokenInstance>(FindObjectsInactive.Include, FindObjectsSortMode.None);
         }
 
         void Awake()
@@ -44,30 +33,15 @@ namespace Platformer.Mechanics
         {
             if (Time.time - nextFrameTime > (1f / frameRate))
             {
-                for (var i = 0; i < tokens.Length; i++)
+                foreach (var token in tokens)
                 {
-                    var token = tokens[i];
-
-                    if (token != null && token._renderer != null && token.sprites != null && token.sprites.Length > 0)
+                    if (token != null && token._renderer != null && token.sprites.Length > 0 && token.gameObject.activeSelf)
                     {
-                        if (token.frame >= token.sprites.Length)
-                            token.frame = 0;
-
                         token._renderer.sprite = token.sprites[token.frame];
-
-                        if (token.collected && token.frame == token.sprites.Length - 1)
-                        {
-                            token.gameObject.SetActive(false);
-                            tokens[i] = null;
-                        }
-                        else
-                        {
-                            token.frame = (token.frame + 1) % token.sprites.Length;
-                        }
+                        token.frame = (token.frame + 1) % token.sprites.Length;
                     }
                 }
-
-                nextFrameTime += 1f / frameRate;
+                nextFrameTime = Time.time;
             }
         }
     }
--- Assets/Scripts/Mechanics/TokenInstance.cs
+++ Assets/Scripts/Mechanics/TokenInstance.cs
@@ -4,26 +4,16 @@ using static Platformer.Core.Simulation;
 
 namespace Platformer.Mechanics
 {
-    /// <summary>
-    /// This class contains the data required for implementing token collection mechanics.
-    /// It does not perform animation of the token, this is handled in a batch by the 
-    /// TokenController in the scene.
-    /// </summary>
     [RequireComponent(typeof(Collider2D))]
     public class TokenInstance : MonoBehaviour
     {
         public AudioClip tokenCollectAudio;
-
-        [Tooltip("If true, animation will start at a random position in the sequence.")]
         public bool randomAnimationStartTime = false;
-
-        [Tooltip("List of frames that make up the animation.")]
         public Sprite[] idleAnimation, collectedAnimation;
 
         internal Sprite[] sprites = new Sprite[0];
         internal SpriteRenderer _renderer;
 
-        // Unique index which is assigned by the TokenController in a scene.
         internal int tokenIndex = -1;
         internal TokenController controller;
         internal int frame = 0;
@@ -32,7 +22,6 @@ namespace Platformer.Mechanics
         void Awake()
         {
             _renderer = GetComponent<SpriteRenderer>();
-
             sprites = idleAnimation;
 
             if (randomAnimationStartTime && sprites.Length > 0)
@@ -50,24 +39,21 @@ namespace Platformer.Mechanics
         {
             if (collected) return;
 
-            // 즉시 보이지 않도록 렌더러 끄기
-            _renderer.enabled = false;
-
-            // 프레임 초기화 및 애니메이션 전환
+            collected = true;
             frame = 0;
             sprites = collectedAnimation;
 
-            if (controller != null)
-                collected = true;
+            if (_renderer != null)
+                _renderer.sprite = collectedAnimation.Length > 0 ? collectedAnimation[0] : null;
 
-            // 사운드 재생 (선택)
             if (tokenCollectAudio != null)
                 AudioSource.PlayClipAtPoint(tokenCollectAudio, transform.position);
 
-            // UI Manager 호출
             Object.FindFirstObjectByType<UIManager>()?.AddToken();
 
-            // 이벤트 전송
+            // 바로 비활성화
+            gameObject.SetActive(false);
+
             var ev = Schedule<PlayerTokenCollision>();
             ev.token = this;
             ev.player = player;
@@ -77,8 +63,9 @@ namespace Platformer.Mechanics
         {
             collected = false;
             sprites = idleAnimation;
+            frame = 0;
             _renderer.enabled = true;
+            gameObject.SetActive(true);
         }
-
     }
 }
